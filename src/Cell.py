9b4f8e2f4e415314e22b61ec0f4fdbc43d17d5c1
anxiousss <saimonkheresh@gmail.com>
2024-03-18 22:08:07 +0300

    add changes due to code review

--- src/Cell.py
+++ src/Cell.py
@@ -1,9 +1,10 @@
 from pygame.locals import Rect
-from pygame import mouse
+import pygame
+from typing import Callable
 
 
 class Coord:
-    def __init__(self, x=0, y=0):
+    def __init__(self, x: int = 0, y: int = 0) -> None:
         self.x, self.y = x, y
 
     def __str__(self):
@@ -29,23 +30,37 @@ class Coord:
 
 
 class Cell:
-    def __init__(self, content=0, weight=0, x=0, y=0):
-        self.position = x, y
+    def __init__(self, content: int = 0, weight: int = 0, coord: Coord = Coord()) -> None:
+        """
+
+        :param content Content in the cell:
+        :param weight:
+        :param coord Coordinates of the cell:
+        """
+        self.position = coord.x, coord.y
         self.content = content
         self.weight = weight
-        self.index = x * 8 + y
+        self.index = coord.x + coord.y * 8
         self.possible_moves = []
 
 
 class Clickable:
-    def __init__(self, onClick, *args, hitbox=Rect(0, 0, 0, 0)):
+    def __init__(self, onClick: Callable, *args, hitbox: Rect = pygame.Rect(0, 0, 0, 0)) -> None:
+        """
+        Clickable (property)
+        :param onClick (callback function):
+        :param *args (arguments for callback function):
+        :param  hitbox (rectangular):
+        """
         self.onClick = onClick
         self.args = args
         self.hitbox = hitbox
 
-    def process(self):
-        """checks collision and calls callback"""
-        mouse_x, mouse_y = mouse.get_pos()
-        if self.hitbox[0] <= mouse_x <= self.hitbox[0] + self.hitbox[2] and \
-                self.hitbox[1] <= mouse_y <= self.hitbox[1] + self.hitbox[3]:
-            self.onClick(self.args)
+    def process(self) -> None:
+        """
+        checks collision and calls callback
+        """
+        point = pygame.mouse.get_pos()
+        collide = self.hitbox.collidepoint(point)
+        if collide:
+            self.onClick(*self.args)
