375dde9289c69b0d41e04fb2d46f7a5368f4aa23
Ecodrin <matvfo@ya.ru>
2024-03-18 22:09:40 +0300

    update board

--- src/board.py
+++ src/board.py
@@ -2,16 +2,18 @@ from Cell import Coord, Cell
 
 
 class Board:
-    def __init__(self, white_amount=0, black_amount=0) -> None:
+    def __init__(self, white_amount: int = 0, black_amount: int = 0) -> None:
         self.white_amount = white_amount
         self.black_amount = black_amount
         self.board = []
+        self.create_board()
 
     def create_board(self) -> None:
         """
-        Создание массива заполненного объектами типа
+        Очистка и заполнение массива объектами типа Cell
         :return:
         """
+        self.board.clear()
         for y in range(8):
             for x in range(8):
                 self.board.append(Cell(Coord(x, 7 - y)))
