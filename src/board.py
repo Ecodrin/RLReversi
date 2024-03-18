1eb203959bd7a9ce1040ecedb99b8d79863ab1e1
Ecodrin <matvfo@ya.ru>
2024-03-18 19:05:38 +0300

    update field

--- src/board.py
+++ src/board.py
@@ -1,18 +1,17 @@
-#from Cell import Coord, Cell
-from pygame.locals import Rect
+from Cell import Coord, Cell
 
 
 class Board:
-    def __init__(self, white_amount=0, black_amount=0):
+    def __init__(self, white_amount=0, black_amount=0) -> None:
         self.white_amount = white_amount
         self.black_amount = black_amount
         self.board = []
 
     def create_board(self) -> None:
         """
-        Созданеи массива заполненного объектами типа
+        Создание массива заполненного объектами типа
         :return:
         """
         for y in range(8):
             for x in range(8):
-                self.board(Cell(Coord(x, 7 - y)))
+                self.board.append(Cell(Coord(x, 7 - y)))
