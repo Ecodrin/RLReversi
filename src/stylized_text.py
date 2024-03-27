a5c16ee52481f01aaa0c68468d94e92d87f70bba
anxiousss <saimonkheresh@gmail.com>
2024-03-27 19:19:35 +0300

    changes to stylized_text due to code review

--- src/stylized_text.py
+++ src/stylized_text.py
@@ -2,51 +2,50 @@ import pygame
 
 
 class StylizedText:
-    def __init__(self, content: str = 'Hello world', Rect: pygame.Rect = (), TextColour: tuple = (0, 0, 0),
-                 FontFamily: str = 'Arial', FontSize: int = 24, FontStyle: int = 0) -> None:
+    def __init__(self, content: str = 'Hello world', position: pygame.Rect = (), text_colour: pygame.color = (0, 0, 0),
+                 font_family: str = 'Arial', font_size: int = 24, font_style: int = 0) -> None:
         """
         :param content: Содержимое текста
-        :param Rect: Позиция кнопки
-        :param TextColour: Цвет текста
-        :param FontFamily: Шрифт текста
-        :param FontSize: Размер текста
-        :param FontStyle: Стиль текста
+        :param position: Позиция текст
+        :param text_colour: Цвет текста
+        :param font_family: Шрифт текста
+        :param font_size: Размер текста
+        :param font_style: Стиль текста
         :return:
         """
 
         self.content = content
-        self.Rect = Rect
-        self.TextColour = TextColour
-        self.FontFamily = FontFamily
-        self.FontSize = FontSize
-        self.FontStyle = FontStyle
+        self.position = position
+        self.text_colour = text_colour
+        self.font_family = font_family
+        self.font_size = font_size
+        self.font_style = font_style
 
-    def __Is_bold(self):
-        """
-        :return: Будет ли шрифт жирным
-        """
-        return self.FontSize % 10
+    def __styles_determinate(self):
+        mask = self.font_style & 0b11
+        match mask:
+            case 0:
+                return False, False
+            case 1:
+                return True, False
+            case 2:
+                return False, True
+            case 3:
+                return True, True
 
-    def __Is_italic(self):
-        """
-        :return: Будет ли шрифт курсивным
-        """
-        return self.FontSize / 10 % 10
-
-    def render(self, screen) -> None:
+    def render(self, screen: pygame.display) -> None:
         """
         Отображает текст с заданным стилем и позицией
         :param screen: разрешение экрана
         """
-        bold = self.__Is_bold()
-        italic = self.__Is_italic()
-        font = pygame.font.SysFont(self.FontFamily, self.FontSize, bold=bold, italic=italic)
-        text_surface = font.render(self.content, True, self.TextColour)
-        screen.blit(text_surface, self.Rect)
+        bold, italic = self.__styles_determinate()
+        font = pygame.font.SysFont(self.font_family, self.font_size, bold=bold, italic=italic)
+        text_surface = font.render(self.content, True, self.text_colour)
+        screen.blit(text_surface, self.position)
 
     def __repr__(self):
-        return (f'StylizedText({self.content}, {self.Rect} ,{self.TextColour}, '
-                f'{self.FontFamily}, {self.FontSize}, {self.FontStyle})')
+        return (f'StylizedText({self.content}, {self.position} ,{self.text_colour}, '
+                f'{self.font_family}, {self.font_size}, {self.font_style})')
 
     def __str__(self):
-        return f'("{self.content}", {self.FontSize}, {self.TextColour})'
+        return f'("{self.content}", {self.font_size}, {self.text_colour})'
