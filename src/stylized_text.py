ef4bfee12f2be127604dda75aa87361c1eb407e1
anxiousss <saimonkheresh@gmail.com>
2024-03-27 23:35:21 +0300

    fix class methods

--- src/stylized_text.py
+++ src/stylized_text.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class StylizedText:
-    def __init__(self, content: str = 'Hello world', position: pygame.Rect = (), text_colour: pygame.color = (0, 0, 0),
+    def __init__(self, content: str = '', position: pygame.Rect = (), text_colour: pygame.color = (0, 0, 0),
                  font_family: str = 'Arial', font_size: int = 24, font_style: int = 0) -> None:
         """
         :param content: Содержимое текста
@@ -10,7 +10,7 @@ class StylizedText:
         :param text_colour: Цвет текста
         :param font_family: Шрифт текста
         :param font_size: Размер текста
-        :param font_style: Стиль текста
+        :param font_style: Стиль текста. Задаётся битовой маской: 1 - жирный 10 - курсив.
         :return:
         """
 
@@ -21,27 +21,24 @@ class StylizedText:
         self.font_size = font_size
         self.font_style = font_style
 
-    def __styles_determinate(self) -> tuple:
+    def __is_cursive(self) -> int:
         """
-        :return: Возвращает значения для отображения нужных стилей текста
+        :return: Разряд отвечающий за курсив.
         """
-        mask = self.font_style & 0b11
-        match mask:
-            case 0:
-                return False, False
-            case 1:
-                return True, False
-            case 2:
-                return False, True
-            case 3:
-                return True, True
+        return self.font_style & 0b10
+
+    def __is_bold(self) -> int:
+        """
+        :return: Разряд отвечающий за жирность.
+        """
+        return self.font_style & 0b01
 
     def render(self, screen: pygame.display) -> None:
         """
         Отображает текст с заданным стилем и позицией
         :param screen: разрешение экрана
         """
-        bold, italic = self.__styles_determinate()
+        bold, italic = self.__is_bold(), self.__is_cursive()
         font = pygame.font.SysFont(self.font_family, self.font_size, bold=bold, italic=italic)
         text_surface = font.render(self.content, True, self.text_colour)
         screen.blit(text_surface, self.position)
