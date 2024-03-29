75d66cb3552b97d795e65684fd6d9d0c3ca583fc
anxiousss <saimonkheresh@gmail.com>
2024-03-29 21:05:25 +0300

    Button class add and StylizedText changed

--- src/stylized_text.py
+++ src/stylized_text.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class StylizedText:
-    def __init__(self, content: str = '', position: pygame.Rect = (), text_colour: pygame.color = (0, 0, 0),
+    def __init__(self, content: str = '', position: pygame.Rect = (), text_colour: tuple = (0, 0, 0),
                  font_family: str = 'Arial', font_size: int = 24, font_style: int = 0) -> None:
         """
         :param content: Содержимое текста.
@@ -13,13 +13,13 @@ class StylizedText:
         :param font_style: Стиль текста. Задаётся битовой маской: 0b01 - жирный, 0b10 - курсив.
         :return:
         """
-
         self.content: str = content
         self.position: pygame.Rect = position
-        self.text_colour: pygame.color = text_colour
+        self.text_colour: tuple = text_colour
         self.font_family: str = font_family
         self.font_size: int = font_size
         self.font_style: int = font_style
+        self.text: pygame.SurfaceType = self.text_create()
 
     def __is_italic(self) -> int:
         """
@@ -33,15 +33,19 @@ class StylizedText:
         """
         return self.font_style & 0b01
 
+    def text_create(self) -> pygame.SurfaceType:
+        bold, italic = self.__is_bold(), self.__is_italic()
+        font = pygame.font.SysFont(self.font_family, self.font_size, bold=bold, italic=italic)
+        text_surface = font.render(self.content, True, self.text_colour)
+        self.position[2], self.position[3] = text_surface.get_width(), text_surface.get_height()
+        return text_surface
+
     def render(self, screen: pygame.display) -> None:
         """
         Отображает текст с заданным стилем и позицией
         :param screen: Разрешение выводимого окна.
         """
-        bold, italic = self.__is_bold(), self.__is_italic()
-        font = pygame.font.SysFont(self.font_family, self.font_size, bold=bold, italic=italic)
-        text_surface = font.render(self.content, True, self.text_colour)
-        screen.blit(text_surface, self.position)
+        screen.blit(self.text, self.position)
 
     def __repr__(self):
         return (f'StylizedText({self.content}, {self.position} ,{self.text_colour}, '
