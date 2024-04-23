24f8d84a778b677b433f416b96ff819019835ac8
Ecodrin <matvfo@ya.ru>
2024-04-23 21:27:54 +0300

    VP-28 | refactor(gymnasium environment): fixed syntax/logic errors

--- env/tictactoe_gym/tictactoe_gym.py
+++ env/tictactoe_gym/tictactoe_gym.py
@@ -2,8 +2,8 @@ from typing import SupportsFloat, Any
 import random
 
 import gymnasium
-from gymnasium import spaces
 import numpy as np
+from gymnasium import spaces
 from gymnasium.core import ActType, ObsType
 
 from src.board import Board
@@ -12,45 +12,49 @@ from src.tictactoe import TicTacToeManager
 
 
 class TicTacToeEnv(gymnasium.Env):
-    def __init__(self, pieces: int = 3, pieces_to_win: int = 3):
-        self.count_cell = pieces
-        self.observation_space: spaces.Box = spaces.Box(low=-1, high=1, shape=(pieces, pieces), dtype=np.int8)
-        self.manager: TicTacToeManager = TicTacToeManager(Board(pieces), pieces_to_win)
+    def __init__(self, size: int = 3, pieces_to_win: int = 3, depht: int = 1):
+        self.depht = depht
+        self.size = size
+        self.manager: TicTacToeManager = TicTacToeManager(Board(size), pieces_to_win)
         self.adversary: Adversary = Adversary(self.manager)
-        self.action_space: ActType = ActionSpace(self.manager.find_legal_moves())
+        self.action_space: ActType = ActionSpaceLegalMoves(self.manager.find_legal_moves())
+        self.observation_space = gymnasium.spaces.Box(low=-1, high=1, shape=(1, self.size ** 2), dtype=np.int8)
 
     def reset(self, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
         self.manager.reset_board()
         self.action_space.legal_moves = self.manager.find_legal_moves()
+        self.observation_space = gymnasium.spaces.Box(low=-1, high=1, shape=(1, self.size ** 2), dtype=np.int8)
         return self.manager.board.board, {}
 
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+        """
+        :param action: Клетка, куда ходит модель
+        :return: (поле, оценка, победа(bool), False, информация о шаге)
+        """
         self.manager.make_move(action)
         reward = self.manager.has_game_ended()
         if reward is not None:
-            return self.manager.board.board, reward, True, False, {}
+            return self.manager.board.board, reward, True, False, {'step': action, 'win': False, 'reward': reward}
         # Умный ход ботяры(глубина выбирается по тому, что вам нужно)
-        self.manager.make_move(self.adversary.search_root(1))
+        self.manager.make_move(self.adversary.search_root(self.depht))
         self.action_space.legal_moves = self.manager.find_legal_moves()
         reward = self.manager.has_game_ended()
-        terminated = True if reward else False
-        return self.manager.board.board, reward, terminated, False, {}
+        terminated = True if reward is not None else False
+        return (self.manager.board.board, reward, terminated, False,
+                {'step': action, 'win': terminated, 'reward': reward})
 
     def render(self, mode='human') -> None:
-        if mode == 'human':
-            self.display()
-
-    def display(self) -> None:
-        for i in range(int(len(self.manager.board.board) ** 0.5)):
-            for j in range(int(len(self.manager.board.board) ** 0.5)):
-                print(f'{self.manager.board.board[i * int(len(self.manager.board.board) ** 0.5) + j]: >2} ', end='')
-            print()
+        match mode:
+            case 'human':
+                display(self.manager.board.board)
+            case _:
+                raise TypeError(f'mode {mode} not supported')
 
     def close(self):
         return 0
 
 
-class ActionSpace(spaces.Space[np.ndarray]):
+class ActionSpaceLegalMoves(spaces.Space):
     def __init__(self, legal_moves: list[int]):
         super().__init__()
         self.legal_moves = legal_moves
@@ -58,3 +62,10 @@ class ActionSpace(spaces.Space[np.ndarray]):
     def sample(self, mask: Any | None = None) -> int:
         move = random.choice(self.legal_moves)
         return move
+
+
+def display(board: list[int]) -> None:
+    for i in range(int(len(board) ** 0.5)):
+        for j in range(int(len(board) ** 0.5)):
+            print(f'{board[i * int(len(board) ** 0.5) + j]: >2} ', end='')
+        print()
