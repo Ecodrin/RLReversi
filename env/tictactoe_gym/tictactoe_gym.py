da07bc29dc515756732e84c5bcbbb246cfb6e2dd
Ecodrin <matvfo@ya.ru>
2024-04-24 20:49:38 +0300

    VP- 28 | fix(environment): fixed syntax errors

--- env/tictactoe_gym/tictactoe_gym.py
+++ env/tictactoe_gym/tictactoe_gym.py
@@ -12,18 +12,17 @@ from src.tictactoe import TicTacToeManager
 
 
 class TicTacToeEnv(gymnasium.Env):
-    def __init__(self, size: int = 3, pieces_to_win: int = 3, depht: int = 1):
-        self.depht = depht
+    def __init__(self, size: int = 3, pieces_to_win: int = 3, depth: int = 1):
+        self.depth = depth
         self.size = size
         self.manager: TicTacToeManager = TicTacToeManager(Board(size), pieces_to_win)
         self.adversary: Adversary = Adversary(self.manager)
-        self.action_space: ActType = ActionSpaceLegalMoves(self.manager.find_legal_moves())
-        self.observation_space = gymnasium.spaces.Box(low=-1, high=1, shape=(1, self.size ** 2), dtype=np.int8)
+        self.action_space: ActType = MoveSpace(self.manager.find_legal_moves())
+        self.observation_space: ObsType = gymnasium.spaces.Box(low=-1, high=1, shape=(self.size ** 2,), dtype=np.int8)
 
     def reset(self, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
         self.manager.reset_board()
         self.action_space.legal_moves = self.manager.find_legal_moves()
-        self.observation_space = gymnasium.spaces.Box(low=-1, high=1, shape=(1, self.size ** 2), dtype=np.int8)
         return self.manager.board.board, {}
 
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
@@ -36,7 +35,7 @@ class TicTacToeEnv(gymnasium.Env):
         if reward is not None:
             return self.manager.board.board, reward, True, False, {'step': action, 'win': False, 'reward': reward}
         # Умный ход ботяры(глубина выбирается по тому, что вам нужно)
-        self.manager.make_move(self.adversary.search_root(self.depht))
+        self.manager.make_move(self.adversary.search_root(self.depth))
         self.action_space.legal_moves = self.manager.find_legal_moves()
         reward = self.manager.has_game_ended()
         terminated = True if reward is not None else False
@@ -46,15 +45,15 @@ class TicTacToeEnv(gymnasium.Env):
     def render(self, mode='human') -> None:
         match mode:
             case 'human':
-                display(self.manager.board.board)
+                display(self.manager.board.board, self.size)
             case _:
-                raise TypeError(f'mode {mode} not supported')
+                raise TypeError(f'Mode "{mode}" is not supported.')
 
     def close(self):
         return 0
 
 
-class ActionSpaceLegalMoves(spaces.Space):
+class MoveSpace(spaces.Space):
     def __init__(self, legal_moves: list[int]):
         super().__init__()
         self.legal_moves = legal_moves
@@ -64,8 +63,14 @@ class ActionSpaceLegalMoves(spaces.Space):
         return move
 
 
-def display(board: list[int]) -> None:
-    for i in range(int(len(board) ** 0.5)):
-        for j in range(int(len(board) ** 0.5)):
+def display(board: list[int], size: int = 3) -> None:
+    """
+    Display board.
+    :param board: board
+    :param size: size of the board
+    :return:
+    """
+    for i in range(size):
+        for j in range(size):
             print(f'{board[i * int(len(board) ** 0.5) + j]: >2} ', end='')
         print()
