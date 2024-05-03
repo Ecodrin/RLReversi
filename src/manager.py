9788328318afb263adee54d9eda1477a70b97f02
MrDemmy <66142897+MrDemmy@users.noreply.github.com>
2024-05-03 16:05:34 +0300

    VP-13 | feature(game logic): added randomness for the best moves

--- src/manager.py
+++ src/manager.py
@@ -1,3 +1,4 @@
+import random
 from abc import ABC, abstractmethod
 
 from src.board import Board
@@ -35,12 +36,12 @@ class Adversary:
 
     # Возвращает текущую оценку позиции. Чем меньше ходов до победы - тем выше оценка.
     def search(self, depth: int, alpha=float('-infinity'), beta=float('+infinity')) -> float:
-        if depth == 0:
-            return 0
-
         win = self.manager.check_win()
         if win:
-            return win + depth
+            return win
+
+        if depth == 0:
+            return 0
 
         available_moves = self.manager.find_legal_moves()
 
@@ -66,16 +67,17 @@ class Adversary:
     def search_root(self, depth: int) -> int:
         moves: list[int] = self.manager.find_legal_moves()
         best_eval = float('-infinity')
-        best_move = None
+        best_possible_moves = []
 
         for move in moves:
             self.manager.make_move(move)
-
-            evaluation = self.search(depth - 1)
+            evaluation = -self.search(depth - 1)
 
             self.manager.unmake_move(move)
 
             if evaluation > best_eval:
                 best_eval = evaluation
-                best_move = move
-        return best_move
+                best_possible_moves.clear()
+            if evaluation == best_eval:
+                best_possible_moves.append(move)
+        return random.choice(best_possible_moves)
