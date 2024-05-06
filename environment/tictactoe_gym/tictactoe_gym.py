23f641088149a76bf5ca7c95eb9a4b4c38d0bbe1
Ecodrin <matvfo@ya.ru>
2024-05-06 23:30:19 +0300

    VP-28 | fix(environment): not fixed major bug

--- environment/tictactoe_gym/tictactoe_gym.py
+++ environment/tictactoe_gym/tictactoe_gym.py
@@ -11,21 +11,25 @@ from src.tictactoe import TicTacToeManager
 
 
 class TicTacToeEnv(gymnasium.Env):
-    def __init__(self, size: int = 3, pieces_to_win: int = 3, depth: int = 5):
+    metadata = {'render_modes': ['human']}
+
+    def __init__(self, size: int = 3, pieces_to_win: int = 3, depth: int = 5, render_mode='human'):
         self.depth = depth
         self.size = size
         self.manager: TicTacToeManager = TicTacToeManager(Board(size), pieces_to_win)
         self.adversary: Adversary = Adversary(self.manager)
         self.action_space: ActType = MoveSpace(self.manager.find_legal_moves())
         self.observation_space: ObsType = gymnasium.spaces.Box(low=-1, high=1, shape=(self.size ** 2,), dtype=int)
+        self.render_mode = render_mode
 
     def reset(self, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[ObsType, dict[str, Any]]:
         self.manager.reset_board()
         self.action_space.legal_moves = self.manager.find_legal_moves()
         # делаем первый ход(чтобы модель ходила вторая)
-        first_step = self.adversary.search_root(self.depth)
-        self.action_space.legal_moves.remove(first_step)
-        self.manager.make_move(first_step)
+        first_move = self.adversary.search_root(self.depth)
+        self.action_space.legal_moves.remove(first_move)
+        self.manager.make_move(first_move)
+        self.render()
         return np.array(self.manager.board.board), {}
 
     def step(self, action: ActType) -> tuple[ObsType, SupportsFloat, bool, bool, dict[str, Any]]:
@@ -35,25 +39,30 @@ class TicTacToeEnv(gymnasium.Env):
         """
         self.manager.make_move(action)
         reward = self.manager.check_win()
+        self.render()
         if reward is not None:
             return (np.array(self.manager.board.board), -reward,
                     True, False, {'step': action, 'win': True, 'reward': -reward})
         # Умный ход ботяры(глубина выбирается по тому, что вам нужно)
-        self.manager.make_move(self.adversary.search_root(self.depth))
+        move = self.adversary.search_root(self.depth)
+        self.manager.make_move(move)
         self.action_space.legal_moves = self.manager.find_legal_moves()
         reward = self.manager.check_win()
         terminated = True if reward is not None else False
+        self.render()
         if reward is None:
-            reward = 1
+            reward = 0
         return (np.array(self.manager.board.board), -reward, terminated, False,
                 {'step': action, 'win': terminated, 'reward': -reward})
 
-    def render(self, mode='human') -> None:
-        match mode:
+    def render(self) -> None:
+        match self.render_mode:
             case 'human':
                 display(self.manager.board.board, self.size)
+            case None:
+                pass
             case _:
-                raise TypeError(f'Mode "{mode}" is not supported.')
+                raise TypeError(f'Mode "{self.render_mode}" is not supported.')
 
     def close(self):
         return 0
@@ -81,3 +90,4 @@ def display(board: list[int], size: int = 3) -> None:
         for j in range(size):
             print(f'{board[i * size + j]: >2} ', end='')
         print()
+    print('----------')
