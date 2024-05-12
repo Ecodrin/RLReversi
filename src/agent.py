ac0b562801425c44fc8d6df8e32bb0a713333f72
MrDemmy <66142897+MrDemmy@users.noreply.github.com>
2024-05-12 15:30:37 +0300

    VP-30 | fix(agent): fixed minor issues

--- src/agent.py
+++ src/agent.py
@@ -23,7 +23,7 @@ class TicTacToeAgent:
             discount_factor: float = 0.95
     ):
         self.env = env
-        self.q_values = defaultdict(self.npzeros)
+        self._q_values = defaultdict(self.npzeros)
 
         self.lr = learning_rate
         self.discount_factor = discount_factor
@@ -32,12 +32,10 @@ class TicTacToeAgent:
         self.epsilon_decay = epsilon_decay
         self.final_epsilon = final_epsilon
 
-        self.training_error = []
-
     def npzeros(self):
         return np.zeros(self.env.action_space.n)
 
-    def get_action(self, obs: int) -> int:
+    def _get_action(self, obs: int) -> int:
         """
         Returns the best action with probability (1 - epsilon)
         otherwise a random action with probability epsilon to ensure exploration.
@@ -47,31 +45,29 @@ class TicTacToeAgent:
             return self.env.action_space.sample()
         # with probability (1 - epsilon) act greedily (exploit)
         else:
-            return int(np.argmax(self.q_values[obs]))
+            return int(np.argmax(self._q_values[obs]))
 
-    def update(
+    def _update(
             self,
             obs: int,
             action: int,
             reward: float,
-            terminated: bool,
             next_obs: int,
     ):
 
-        future_q_value = (not terminated) * (self.q_values[next_obs]).max()
+        future_q_value = (self._q_values[next_obs]).max()
         temporal_difference = (
-                reward + self.discount_factor * future_q_value - self.q_values[obs][action]
+                reward + self.discount_factor * future_q_value - self._q_values[obs][action]
         )
 
-        self.q_values[obs][action] = (
-                self.q_values[obs][action] + self.lr * temporal_difference
+        self._q_values[obs][action] = (
+                self._q_values[obs][action] + self.lr * temporal_difference
         )
-        self.training_error.append(temporal_difference)
 
-    def decay_epsilon(self):
+    def _decay_epsilon(self):
         self.epsilon = max(self.final_epsilon, self.epsilon - self.epsilon_decay)
 
-    def learn(self, total_episodes=50_000):
+    def learn(self, total_episodes: int = 50_000) -> None:
         if self.epsilon_decay is None:
             self.epsilon_decay = self.epsilon / (total_episodes / 2)
 
@@ -81,32 +77,32 @@ class TicTacToeAgent:
 
             # play one episode
             while not done:
-                action = self.get_action(obs)
+                action: int = self._get_action(obs)
                 next_obs, reward, terminated, truncated, info = self.env.step(action)
 
                 # update the agent
-                self.update(obs, action, reward, terminated, next_obs)
+                self._update(obs, action, reward, next_obs)
 
                 # update if the environment is done and the current obs
-                done = terminated or truncated
+                done: bool = terminated or truncated
                 obs = next_obs
 
-            self.decay_epsilon()
+            self._decay_epsilon()
 
     # mask is a list of current legal moves
-    def predict(self, obs: int, mask: list[int]):
+    def predict(self, obs: int, mask: list[int]) -> tuple[np.ndarray, int]:
         # artificially remove illegal moves and randomly choose one of the best moves
-        temp = self.q_values[obs].copy()
+        temp: np.ndarray = self._q_values[obs].copy()
         temp[mask] += 10
         return temp, random.choice(np.where(temp.max() == temp)[0])
 
-    def save(self, path: os.PathLike | str):
+    def save(self, path: os.PathLike | str) -> None:
         with open(path, 'wb') as f:
             pickle.dump(self, f)
 
     @staticmethod
     def load(path: os.PathLike | str):
         with open(path, 'rb') as f:
-            instance = pickle.load(f)
-            instance.q_values = defaultdict(instance.npzeros, instance.q_values)
+            instance: TicTacToeAgent = pickle.load(f)
+            instance._q_values = defaultdict(instance.npzeros, instance._q_values)
             return instance
